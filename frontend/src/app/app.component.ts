746aa8956c328d58b4f0005f34096f37bcdb197b
MatteoPanicciari <matteo.panicciari@gmail.com>
2024-10-14 21:44:33 +0200

    ancora routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { SelfieComponent } from './component/selfie/selfie.component';
+import { LoginComponent } from "./component/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SelfieComponent, LoginComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        console.log('app');
+    }
+
+    title = 'fronted';
 }
